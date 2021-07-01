577704d6e476cdb845223b73942cf18b5df909a0
Ajay Reddy <ajayreddychinthala110125@gmail.com>
2021-07-01 16:07:39 +0530

    .

--- week7/add_matrix.c
+++ week7/add_matrix.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 int main()
 {
-	int m,n,c,d,first[10][10],second[10][10],sum[10][10];
+	int m, n, c, d, first[10][10], second[10][10], sum[10][10];
 	printf("enter the no of rows and columns of the matrix\n");
-	scanf("%d%d",&m,&n);
+	scanf("%d%d", &m, &n);
 	printf("enter the elements of the first matrix\n");
-	for(c=0;c<m;c++)
-	for(d=0;d<n;d++)
-	scanf("%d",&first[c][d]);
+	for (c = 0; c < m; c++)
+		for (d = 0; d < n; d++)
+			scanf("%d", &first[c][d]);
 	printf("enter the elements of the second matrix\n");
-	for(c=0;c<m;c++)
-	for(d=0;d<n;d++)
-	scanf("%d",&second[c][d]);
-	for(c=0;c<m;c++)
-	for(d=0;d<n;d++)
-	sum[c][d]=first[c][d]+second[c][d];
+	for (c = 0; c < m; c++)
+		for (d = 0; d < n; d++)
+			scanf("%d", &second[c][d]);
+	for (c = 0; c < m; c++)
+		for (d = 0; d < n; d++)
+			sum[c][d] = first[c][d] + second[c][d];
 	printf("the sum of the matrix is:\n");
-	for(c=0;c<m;c++)
-	for(d=0;d<n;d++)
-	printf("%d\t",sum[c][d]);
+	for (c = 0; c < m; c++)
+		for (d = 0; d < n; d++)
+			printf("%d\t", sum[c][d]);
 	printf("\n");
 	return 0;
 }
--- week7/sting_palin.c
+++ week7/sting_palin.c
@@ -3,20 +3,20 @@
 int main()
 {
 	char str[100];
-	int i,len,c=0;
+	int i, len, c = 0;
 	printf("enter a string:");
 	gets(str);
-	len=strlen(str);
-	for(i=0;str[i]!='\0';i++)
+	len = strlen(str);
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if(str[i]!=str[len-1-i])
-	{
-		c=1;
-		break;
+		if (str[i] != str[len - 1 - i])
+		{
+			c = 1;
+			break;
+		}
 	}
-}
-	if(c==1)
-	printf("not a polidrone");
+	if (c == 1)
+		printf("not a polidrone");
 	else
-	printf("polindrone");
+		printf("polindrone");
 }
