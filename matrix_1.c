ea0e7e6897ef528b9df59c872e46a3ef0e5880aa
Ajay Reddy <ajayreddychinthala110125@gmail.com>
2021-08-06 21:10:15 +0530

    inverse done

--- matrix_1.c
+++ matrix_1.c
@@ -1,46 +1,143 @@
 #include <stdio.h>
-int add(int x,int y);
-int add(int x,int y){
-    int sum;
-    sum = x +y;
-    return sum;
-}
+#include <stdlib.h>
 
 
-void  add_matrix(int a_row, int a_col, int a[a_row][a_col],int b[a_row][a_col],int  sum[a_row][a_col]);
-void  add_matrix(int a_row, int a_col, int a[a_row][a_col],int b[a_row][a_col],int  sum[a_row][a_col]){
+int ** add_matrix(int a_row, int a_col, int a[a_row][a_col],int b[a_row][a_col]);
+int ** add_matrix(int a_row, int a_col, int a[a_row][a_col],int b[a_row][a_col]){
     int m,n;
+    int **sum;
+    sum = calloc(sizeof(int*),a_row);
+    for (size_t i = 0; i < a_row; i++)
+    {
+        sum[i] = calloc(sizeof(int*),a_col);
+    }
+    
     for (m = 0; m < a_row; m++){
         for (n = 0; n < a_col; n++){
             sum[m][n] = a[m][n] +b[m][n];
         }
     }
+    return sum;
 }
 
-void  sub_matrix(int a_row, int a_col, int a[a_row][a_col],int b[a_row][a_col],int  sub[a_row][a_col]);
-void  sub_matrix(int a_row, int a_col, int a[a_row][a_col],int b[a_row][a_col],int  sub[a_row][a_col]){
+
+int ** sub_matrix(int a_row, int a_col, int a[a_row][a_col],int b[a_row][a_col]);
+int ** sub_matrix(int a_row, int a_col, int a[a_row][a_col],int b[a_row][a_col]){
     int m,n;
+    int **sub;
+    sub = calloc(sizeof(int*),a_row);
+    for (size_t i = 0; i < a_row; i++)
+    {
+        sub[i] = calloc(sizeof(int*),a_col);
+    }
+    
     for (m = 0; m < a_row; m++){
         for (n = 0; n < a_col; n++){
             sub[m][n] = a[m][n] - b[m][n];
         }
     }
+    return sub;
 }
 
-void  mul_matrix(int a_row, int a_col, int a[a_row][a_col],int b[a_row][a_col],int  mul[a_row][a_col]);
-void  mul_matrix(int a_row, int a_col, int a[a_row][a_col],int b[a_row][a_col],int  mul[a_row][a_col]){
+
+int ** mul_matrix(int a_row, int a_col, int b_row, int b_col, int a[a_row][a_col],int b[b_row][b_col]);
+int ** mul_matrix(int a_row, int a_col,int b_row, int b_col, int a[a_row][a_col],int b[b_row][b_col]){
     int m,n;
+    int **mul;
+    mul = calloc(sizeof(int*),b_row);
+    for (size_t i = 0; i < a_row; i++)
+    {
+        mul[i] = calloc(sizeof(int*),a_col);
+    }
+    
     for (m = 0; m < a_row; m++){
         for (n = 0; n < a_col; n++){
-            mul[m][n] = a[n][m] *b[m][n];
+            mul[m][n] = a[m][n] * b[n][m];
         }
+    }
+    return mul;
+}
+
+
+int ** tran_matrix( int row, int col,int a[row][col]);
+
+int ** tran_matrix( int row, int col,int a[row][col]){
+    int **tran;
+    tran = calloc(sizeof(int*),col);
+    for (size_t i = 0; i < row; i++)
+    {
+        tran[i] = calloc(sizeof(int*),row);
+    }
+    for (int i = 0; i < row; i++){
+        for (int j = 0; j < col; j++){
+            tran[j][i] = a[i][j];
+        }
+    }
+    return tran;
+}
+
+
+int  det( int row, int col,int a[row][col]);
+
+int  det( int row, int col,int a[row][col]){
+    int det;
+    if (row == 3){
+        det = a[0][0]*((a[1][1]*a[2][2])-(a[1][2]*a[2][1])) - a[0][1]*((a[1][0]*a[2][2])-(a[1][2]*a[2][0])) +a[0][2]*((a[1][0]*a[2][1])-(a[1][1]*a[2][0]));
+        return det;
+    }
+    if(row == 2){
+        det = a[0][0]*a[1][1] - a[0][1]*a[1][0];
+        return det;
     }
 }
 
+int ** cofactor_matrix( int row, int col,int a[row][col]);
 
+int ** cofactor_matrix( int row, int col,int a[row][col]){
+    int **cofactor;
+    if( row == 3 && col == 3){
+        cofactor = calloc(sizeof(int*),row);
+        for (size_t i = 0; i < row; i++)
+        {
+            cofactor[i] = calloc(sizeof(int*),col);
+        }
+        cofactor[0][0] =  1*(a[1][1]*a[2][2] - a[1][2]*a[2][1]);
+        cofactor[0][1] =  -1*(a[1][0]*a[2][2] - a[1][2]*a[2][0]);
+        cofactor[0][2] =  1*(a[1][0]*a[2][1] - a[1][1]*a[2][1]);
+        cofactor[1][0] =  -1*(a[0][1]*a[2][2] - a[0][2]*a[2][0]);
+        cofactor[1][1] =  1*(a[0][0]*a[2][2] - a[0][2]*a[2][0]);
+        cofactor[1][2] =  -1*(a[0][0]*a[2][1] - a[0][1]*a[2][0]);
+        cofactor[2][0] =  1*(a[0][1]*a[1][2] - a[0][2]*a[1][1]);
+        cofactor[2][1] =  -1*(a[0][0]*a[1][2] - a[0][2]*a[1][0]);
+        cofactor[2][2] =  1*(a[0][0]*a[1][1] - a[0][1]*a[1][0]);
+        return cofactor;
+    }
+    
+}
 
-void print_matrix(int row, int col,int arr[row][col]);
-void print_matrix(int row, int col,int arr[row][col]){
+int ** scalar_division(int row, int col,int a[row][col],float scalar);
+int ** scalar_division(int row, int col,int a[row][col],float scalar){
+    int **scalar_div_matrix;
+    scalar_div_matrix = calloc(sizeof(int*),row);
+    for (size_t i = 0; i < row; i++)
+    {
+        scalar_div_matrix[i] = calloc(sizeof(int*),row);
+    }
+    for (size_t i = 0; i < row; i++)
+    {
+        for (size_t j = 0; j < col; j++)
+        {
+            scalar_div_matrix[i][j] = a[i][j]/scalar;
+        }
+        
+    }
+    
+    return scalar_div_matrix;
+    
+
+}
+void print_matrix(int row, int col,int **arr);
+void print_matrix(int row, int col,int **arr){
     int m,n;
     for (m = 0; m < row; m++){
         for (n = 0; n < col; n++){
@@ -55,7 +152,7 @@ void main(){
     scanf("%d %d", &a_row,&a_col);
     printf("Enter The Size of matrix two : \n");
     scanf("%d %d", &b_row,&b_col);
-    int a[a_row][a_col],b[b_row][b_col],sum[a_row][a_col],sub[a_row][a_col];
+    int a[a_row][a_col],b[b_row][b_col],sub[a_row][a_col],mul[a_row][b_col];
    
     printf("ENTER THE ELEMENTS OF MATRIX One :\n");
     for (m = 0; m < a_row; m++){
@@ -69,13 +166,118 @@ void main(){
             scanf("%d", &b[m][n]);
         }
     }
-
-    add_matrix(a_row,a_col, a[a_row][a_col], b[a_row][a_col],  sum[a_row][a_col]);
-    for (m = 0; m < b_row; m++){
-        for (n = 0; n < b_col; n++){
-            printf("%d", sum[m][n]);
+    int opt;
+    printf("[0]exit\n[1]addition\n[2]subtraction\n[3]mul\n[4]tran_a\n[5]tran_b\n[6]det a\n[7]det b\n[8]co-factor of a\n[9]co-factor of b\n[10]inverse of a\n[11]inverse of b\n");
+    while (1)
+    {
+        printf("Enter option:");
+        scanf("%d",&opt);
+        if(opt == 0){
+            break;
+        }
+        else if(opt == 1){
+            printf("\nAddition of matrix");
+            int **add = add_matrix(a_row,a_col,a,b);
+            print_matrix(a_row,a_col, add);
+            free(add);
+            printf("\n");
+            
+        }
+        else if(opt == 2){
+            printf("\nsub of matrix\n");
+            int **sub = sub_matrix(a_row,a_col,a,b);
+            print_matrix(a_row,a_col, sub);
+            free(sub);
+            printf("\n");
+           
+        }
+        else if( opt == 3){
+            printf("\nmul of matrix\n");
+            int **mul = mul_matrix(a_row,a_col,b_row,b_col,a,b);
+            print_matrix(a_row,a_col, mul);
+            free(mul);
+            printf("\n");
+           
+        }
+        else if(opt == 4){
+            printf("\ntran of matrix A\n");
+            int **tran_a = tran_matrix(a_row,a_col,a);
+            print_matrix(a_row,a_col, tran_a);
+            free(tran_a);
+            printf("\n");
+        }
+        else if(opt == 5){
+            printf("\ntran of matrix B\n");
+            int **tran_b = tran_matrix(b_row,b_col,a);
+            print_matrix(a_row,a_col, tran_b);
+            free(tran_b);
+            printf("\n");
+        }
+        else if(opt == 6){
+            printf("\ndet of matrix A\n");
+            if(a_row == a_col){
+                printf("%d",det(a_row,a_col,a));
+                printf("\n");
+            }else{
+                printf("det Not possible.\n");
+            }
+        }
+        else if(opt == 7){
+            printf("\ndet of matrix B\n");
+            if(b_row == b_col){
+                printf("%d",det(b_row,b_col,b));
+                printf("\n");
+            }else{
+                printf("det Not possible.\n");
+            }
+        }
+        else if(opt == 8){
+            printf("\nco-factor of matrix A\n");
+            int ** co_mat = cofactor_matrix(a_row,a_col,a);
+            print_matrix(a_row,a_col,co_mat);
+            free(co_mat);
+            printf("\n");
+        }
+        else if(opt == 9){
+            printf("\nco-factor of matrix B\n");
+            int ** co_mat = cofactor_matrix(b_row,b_col,b);
+            print_matrix(b_row,b_col,co_mat);
+            free(co_mat); 
+            printf("\n");
+        }
+        else if(opt == 10){
+            printf("\ninverse of matrix A\n");
+            int det_a = det(a_row,a_col,a);
+            (float) det_a;
+            int ** co_mat = cofactor_matrix(a_row,a_col,a);
+            int ** adj_mat = tran_matrix(a_row,a_col,co_mat);
+            int ** inverse_matrix = scalar_division(a_row,a_col,adj_mat,det_a);
+            print_matrix(a_row,a_col,inverse_matrix);
+            free(co_mat);
+            free(adj_mat);
+            free(inverse_matrix);
+            printf("\n");
+        }
+        else if(opt == 11){
+            printf("\ninverse of matrix B\n");
+            int det_b = det(b_row,b_col,b);
+            (float) det_b;
+            int ** co_mat = cofactor_matrix(b_row,b_col,b);
+            int ** adj_mat = tran_matrix(b_row,b_col,co_mat);
+            int ** inverse_matrix = scalar_division(b_row,b_col,adj_mat,det_b);
+            print_matrix(a_row,a_col,inverse_matrix);
+            free(co_mat);
+            free(adj_mat);
+            free(inverse_matrix);
+            printf("\n");
+        }
+        else{
+            printf("\n invalid option \n");
         }
     }
+    
+   
+    
 
     
 }
